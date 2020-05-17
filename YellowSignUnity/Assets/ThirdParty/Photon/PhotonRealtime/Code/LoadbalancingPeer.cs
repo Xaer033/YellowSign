c3616f13ecd3958997e37dcb408161f1ceb280a3
Xaer0 <jdfrag@gmail.com>
2020-05-16 21:22:47 -0700

    current progress

--- YellowSignUnity/Assets/ThirdParty/Photon/PhotonRealtime/Code/LoadbalancingPeer.cs
+++ YellowSignUnity/Assets/ThirdParty/Photon/PhotonRealtime/Code/LoadbalancingPeer.cs
@@ -13,7 +13,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using ExitGames.Client.Photon;
-
+using Photon.Realtime;
 #if UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_4_7 || UNITY_5 || UNITY_5_0 || UNITY_5_1 || UNITY_2017 || UNITY_2018 || UNITY_2019
     using UnityEngine;
     using Hashtable = ExitGames.Client.Photon.Hashtable;
